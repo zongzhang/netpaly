897a3aab7ebbb6c0528cdff259fa244223d7b12e
zongzhang <jingchang_007@163.com>
2018-12-09 18:20:23 +0800

    添加代码

--- chapter2/Server/src/main/java/com/zongzhang/EchoServer.java
+++ chapter2/Server/src/main/java/com/zongzhang/EchoServer.java
@@ -35,20 +35,28 @@ public class EchoServer {
 
     public void start() throws Exception {
         final EchoServerHandler serverHandler = new EchoServerHandler();
+        // 1.创建EventLoopGroup
         EventLoopGroup group = new NioEventLoopGroup();
         try {
+            // 2.创建ServerBootstrap
             ServerBootstrap b = new ServerBootstrap();
+            // 3.指定所使用的NIO传输Channel
             b.group(group).channel(NioServerSocketChannel.class)
+                    // 4.使用指定的端口
                     .localAddress(new InetSocketAddress(port))
+                    // 5.添加一个EchoServerHandler到子Channel的ChannelPipeline
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(serverHandler);
                         }
                     });
+            // 6.异步绑定服务器；调用sync()方法直到绑定完成
             ChannelFuture f = b.bind().sync();
+            // 7.获取Channel的CloseFuture，并且阻塞当前线程直到它完成
             f.channel().closeFuture().sync();
         } finally {
+            // 8.关闭EventLoopGroup，释放所有的资源
             group.shutdownGracefully().sync();
         }
     }
